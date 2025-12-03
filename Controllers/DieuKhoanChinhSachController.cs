4ae7b24fcb4f2f1cda87296e990b3216fa5c9e65
hoai nguyen <nguyenhoai2k5myt@gmail.com>
2025-12-03 18:24:20 +0700

    fix: escape backslash in connection string to make appsettings.json valid JSON

---
+++ Controllers/DieuKhoanChinhSachController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KIDORA.Controllers
+{
+    public class DieuKhoanChinhSachController : Controller
+    {
+        public IActionResult ChinhSachGiaoHang()
+        {
+            return View();
+        }
+        public IActionResult ChinhSachBaoMat()
+        {
+            return View();
+        }
+        public IActionResult chinhSachBaoHangVaDoiTra()
+        {
+            return View();
+        }
+        public IActionResult ChinhSachThanhToan()
+        {
+            return View();
+        }
+    }
+}
