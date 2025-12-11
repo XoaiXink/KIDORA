31fe831447fc314a1192779f4fcbcf9898ab466a
miepham2005 <vankhue2005@gmail.com>
2025-12-11 15:26:30 +0700

    Cơ bản hoàn thành chức năng đăng ký đăng nhập (bản 1)

--- Data/KhachHang.cs
+++ Data/KhachHang.cs
@@ -1,41 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace KIDORA.Data;
-
-public partial class KhachHang
+namespace KIDORA.Data
 {
-    public string MaKh { get; set; } = null!;
+    public partial class KhachHang
+    {
+        public string MaKh { get; set; } = null!;
+        public string MaHang { get; set; } = null!;
+        public int SoDuKcoin { get; set; }
+        public DateTime NgayThamGia { get; set; }
 
-    public string HoTen { get; set; } = null!;
+        public virtual NguoiDung MaKhNavigation { get; set; } = null!;
 
-    public string Email { get; set; } = null!;
 
-    public string DienThoai { get; set; } = null!;
+        public virtual HangThanhVien MaHangNavigation { get; set; } = null!;
 
-    public string MatKhauHash { get; set; } = null!;
 
-    public string? GioiTinh { get; set; }
+        public virtual ICollection<DiaChiKhachHang> DiaChiKhachHangs { get; set; }
+            = new List<DiaChiKhachHang>();
 
-    public DateOnly? NgaySinh { get; set; }
+        public virtual ICollection<DonHang> DonHangs { get; set; }
+            = new List<DonHang>();
 
-    public string MaHang { get; set; } = null!;
+        public virtual ICollection<GdKcoin> GdKcoins { get; set; }
+            = new List<GdKcoin>();
 
-    public int SoDuKcoin { get; set; }
+        public virtual ICollection<GioHang> GioHangs { get; set; }
+            = new List<GioHang>();
 
-    public DateTime NgayThamGia { get; set; }
+        public virtual ICollection<Wishlist> Wishlists { get; set; }
+            = new List<Wishlist>();
 
-    public virtual ICollection<DanhGiaSanPham> DanhGiaSanPhams { get; set; } = new List<DanhGiaSanPham>();
-
-    public virtual ICollection<DiaChiKhachHang> DiaChiKhachHangs { get; set; } = new List<DiaChiKhachHang>();
-
-    public virtual ICollection<DonHang> DonHangs { get; set; } = new List<DonHang>();
-
-    public virtual ICollection<GdKcoin> GdKcoins { get; set; } = new List<GdKcoin>();
-
-    public virtual ICollection<GioHang> GioHangs { get; set; } = new List<GioHang>();
-
-    public virtual HangThanhVien MaHangNavigation { get; set; } = null!;
-
-    public virtual ICollection<Wishlist> Wishlists { get; set; } = new List<Wishlist>();
+        public virtual ICollection<DanhGiaSanPham> DanhGiaSanPhams { get; set; }
+            = new List<DanhGiaSanPham>();
+    }
 }
---
+++ Data/NguoiDung.cs
@@ -0,0 +1,22 @@
+﻿namespace KIDORA.Data
+{
+    public partial class NguoiDung
+    {
+        public string Id { get; set; } = null!;
+        public string HoTen { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? DienThoai { get; set; }
+        public string MatKhauHash { get; set; } = null!;
+        public string? GioiTinh { get; set; }
+        public DateTime? NgaySinh { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string? VerificationToken { get; set; }
+        public string? ResetToken { get; set; }
+        public DateTime? ResetTokenExpiry { get; set; }
+
+        public string LoaiNguoiDung { get; set; } = null!;   // 'KHACH_HANG' hoặc 'ADMIN'
+
+        public virtual KhachHang? KhachHang { get; set; }
+        public virtual Admin? Admin { get; set; }
+    }
+}
